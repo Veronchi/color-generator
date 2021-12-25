2a3854a3e2689deee036729ba0cac4e28c143253
Veronchi <zhigunova1995@gmail.com>
2021-12-25 17:27:46 +0300

    add rendering of colors

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = ({color}) => {
-  console.log(color)
+  const[colorHex, setColorHex] = useState('');
+
+  useEffect(() => {
+    setColorHex(rgbToHex(...color.rgb))
+  }, [color])
+  
 
   let colorClass = color.type === 'tint' ? 'false' : 'color-light';
-  
+
   return (
-    <article className={`color ${colorClass}`}>
+    <article className={`color ${colorClass}`} style={{backgroundColor: `rgb(${color.rgb.join()})`}}>
       <p className='percent-value'>{color.weight}%</p>
-      <p className='color-value'></p>
+      <p className='color-value'>{colorHex}</p>
     </article>
   );
 }
